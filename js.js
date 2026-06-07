56c9e8213b6c23a1eb8f90a1e1bf500fa92c9a06
Alessandro Rodrigues de Oliveira <alessandro14736@gmail.com>
2026-06-07 15:18:54 -0300

    Update js.js

--- js.js
+++ js.js
@@ -22,7 +22,7 @@ const db = getFirestore(app);
 /****************************************************************************************** */
 /*Constantes globais*/
 
-let life = 120;
+let life = 100;
 let perguntas = [];
 let pergunta = 0;
 let pontuacao = 0;
